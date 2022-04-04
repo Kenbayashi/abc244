e60d68f862db479fda9c6e2481f60034588a1453
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-04-04 12:05:14 +0900

    b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -1,3 +1,24 @@
+use std::usize;
+
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    unimplemented!();
+    input! {
+        _: usize,
+        t: Chars,
+    }
+
+    let straight = |((x, y), (d_x, d_y)): ((i64, i64), (i64, i64))| {
+        ((x + d_x, y + d_y), (d_x, d_y))
+    };
+
+    let right = |((x, y), (d_x, d_y)): ((i64, i64), (i64, i64))| {
+        ((x, y), (d_y, d_x * -1))
+    };
+
+    let ((x, y), _) = t.into_iter()
+                    .fold(((0, 0), (1, 0)), |acc, c| if c == 'S' {straight(acc)} else {right(acc)});
+
+    println!("{} {}", x, y);
 }
