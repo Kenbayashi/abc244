520c76a42248cf7a272732f25f8f2f68f2c8c456
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-04-04 17:05:11 +0900

    c

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -9,16 +9,12 @@ fn main() {
         t: Chars,
     }
 
-    let straight = |((x, y), (d_x, d_y)): ((i64, i64), (i64, i64))| {
-        ((x + d_x, y + d_y), (d_x, d_y))
-    };
+    let straight = |((x, y), (d_x, d_y))| ((x + d_x, y + d_y), (d_x, d_y));
 
-    let right = |((x, y), (d_x, d_y)): ((i64, i64), (i64, i64))| {
-        ((x, y), (d_y, d_x * -1))
-    };
+    let right = |(pos, (d_x, d_y))| (pos, (d_y, d_x * -1));
 
     let ((x, y), _) = t.into_iter()
-                    .fold(((0, 0), (1, 0)), |acc, c| if c == 'S' {straight(acc)} else {right(acc)});
+                       .fold(((0i64, 0i64), (1i64, 0i64)), |acc, c| if c == 'S' {straight(acc)} else {right(acc)});
 
     println!("{} {}", x, y);
 }
--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    let mut ans = 1i64;
+    let mut used = Vec::<i64>::new();
+
+    loop {
+        while used.contains(&ans) {
+            ans += 1;
+        }
+
+        println!("{}", ans);
+        used.push(ans);
+
+        input! {
+            n: i64,
+        }
+
+        if n != 0 {
+            used.push(n);
+        } else {
+            break;
+        }
+    }
 }
